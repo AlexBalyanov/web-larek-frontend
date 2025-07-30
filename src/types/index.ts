b7b3f871904fd68c2b4e463805fbe08c9199bb95
Alex Balyanov <alex44298@yandex.ru>
2025-07-30 18:22:56 +0500

    описал слой данных в доке

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,5 @@
+import { IEvents } from '../components/base/events';
+
 export interface IProduct {
 	id: string;
 	description: string;
@@ -7,17 +9,50 @@ export interface IProduct {
 	price: number | null;
 }
 
-export interface IProductData {
-	products: IProduct[];
-	preview: string | null;
-	getProduct(id: string): IProduct;
+export interface IProductsData {
+	_products: IProduct[];
+	_selectedProduct: IProduct;
+	events: IEvents;
+	set products(products: IProduct[]);
+	get products(): IProduct[];
+	set selectedProduct(product: IProduct);
+	get selectedProduct(): IProduct;
 }
 
+export interface IUser {
+	payment: PaymentMethod;
+	email: string;
+	phone: string;
+	address: string;
+}
 
-export type ApiPostMethods = 'POST' | 'PUT' | 'DELETE' | 'PATCH';
+export interface IUserData {
+	payment: PaymentMethod;
+	email: string;
+	phone: string;
+	address: string;
+	events: IEvents;
+	setUserData(user: Partial<IUser>): void;
+	getUserData(): IUser;
+	checkUserValidation(): boolean;
+}
+
+export interface IBasketData {
+	products: IProduct[];
+	events: IEvents;
+	getProductsList(): IProduct[];
+	getProductsCount(): number;
+	getProductsPrice(): number;
+	addToBasket(product: IProduct): void;
+	deleteFromBasket(product: IProduct): void;
+	isProductInBasket(id: string): boolean;
+}
 
 export interface IApi {
 	baseUrl: string;
 	get<T>(uri: string): Promise<T>;
 	post<T>(uri: string, data: object, method?: ApiPostMethods): Promise<T>;
 }
+
+export type ApiPostMethods = 'POST' | 'PUT' | 'DELETE' | 'PATCH';
+export type PaymentMethod = 'cash' | 'online' | '';
